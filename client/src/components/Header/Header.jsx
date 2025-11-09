50362fd88f9eb6bbe9072cc9b54e581861c9dc23
Harrison Strand <harrisonstrand@STRAND.local>
2025-11-08 17:00:39 -0700

    add shadows to nav icons, dark/light mode button complete, all main nav styling and css variables

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,43 +1,85 @@
+import { useEffect, useState } from "react";
 import { useContext } from "react";
-import { ThemeContext } from "../../context/ThemeContext";
+import { ThemeContext } from "../../context/ThemeContext.jsx";
 import { Link } from "react-router-dom";
-import './Header.css';
+import "./Header.css";
 
-export default function Header(){
-
+export default function Header() {
 	const { theme, toggleTheme } = useContext(ThemeContext);
+	const [logoSrc, setLogoSrc] = useState("");
 
-  return (
-		<div className="header bg-main">
-			<nav className="navbar navbar-expand-lg container-fluid">
-				<div className="container-fluid row g-0 justify-content-lg-between px-3 py-3">
-					<div className="col-4 col-lg-6">
-						<Link className="fw-semibold text-uppercase d-flex flex-row text-dark align-items-center" to="/">
-							<div className="logo header-logo"></div>
-							<p className="company-title py-0 my-0 px-3 d-sm-none d-md-flex">Industrial Hardware Idaho</p>
+	const updateLogoSrc = () => {
+		const root = document.documentElement;
+		const logoValue = getComputedStyle(root).getPropertyValue("--logo").trim();
+		const match = logoValue.match(/url\((['"]?)(.*?)\1\)/);
+		if (match) setLogoSrc(match[2]);
+	};
+
+	useEffect(() => {
+		updateLogoSrc();
+	}, []);
+
+	useEffect(() => {
+		updateLogoSrc();
+	}, [theme]);
+
+	const handleToggleTheme = () => {
+		toggleTheme();
+		setTimeout(updateLogoSrc, 0);
+	};
+
+	return (
+		<div className='header bg-main'>
+			<nav className='navbar navbar-expand-lg container-fluid'>
+			<button
+				onClick={handleToggleTheme}
+				className='theme-btn rounded-circle d-flex position-absolute'
+				style={{
+					backgroundColor: theme === "light" ? "#000" : "#fff",
+					transition: "background-color 0.3s ease, color 0.3s ease",
+				}}></button>
+				<div className='container-fluid flex-row g-0 justify-content-between px-3 py-2 align-items-center'>
+					<div className='col-6 col-lg-8'>
+						<Link
+							className='text-decoration-none fw-bolder fs-5 text-uppercase d-flex flex-row text-main-light'
+							to='/'>
+							{/* <div className="col-4 col-lg-2 header-logo d-flex mx-4"/> */}
+							<img
+								src={logoSrc}
+								className='header-logo col-4 col-lg-2 d-flex mx-4'
+							/>
+							{/* <div className="col-4 col-lg-2 header-logo d-flex mx-4"/> */}
+							<h2 className='text-main-light align-items-center company-title py-0 my-0 px-2 d-flex'>
+								Industrial Hardware Idaho
+							</h2>
 						</Link>
 					</div>
-					<div className="col-6 text-end d-lg-none d-block">
-						<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navMain">
-							<span className="navbar-toggler-icon" />
+					<div className='col-4 col-lg-2 align-items-center d-flex justify-content-end mx-4'>
+						<Link className='account-link rounded-circle mx-4' to='/register'>
+							<div className='account-thumb rounded-circle' />
+						</Link>
+						<div className='cart-icon py-0 my-0 d-none d-sm-flex text-main-light bi bi-bag h1'></div>
+					</div>
+					<div className='col-2 text-end d-none'>
+						<button
+							className='navbar-toggler'
+							type='button'
+							data-bs-toggle='collapse'
+							data-bs-target='#navMain'>
+							<span className='navbar-toggler-icon' />
 						</button>
-					</div>
-					<div className="col-4 col-lg-2 align-items-center d-flex justify-content-end">
-					<div className="account-icon py-0 my-0 px-3 bi bi-person-circle h2">
-					</div>
-					<div className="cart-icon py-0 my-0 px-3 text-dark bi bi-bag h2">
-					</div>
 					</div>
 				</div>
 			</nav>
-					<div className="collapse navbar-collapse" id="navMain">
-						<ul className="navbar-nav ms-auto gap-lg-2">
-							<li className="nav-item text-uppercase"><Link className="nav-link" to="/products">SHOP NOW</Link></li>
-						</ul>
-					</div>
-		<button onClick={toggleTheme} className="btn btn-outline-light">
-        {theme === "light" ? "Dark" : "Light"} Mode
-      </button>
+			<div className='collapse navbar-collapse' id='navMain'>
+				<ul className='navbar-nav ms-auto gap-lg-2'>
+					<li className='nav-item text-uppercase'>
+						<Link className='nav-link' to='/products'>
+							SHOP NOW
+						</Link>
+					</li>
+				</ul>
+			</div>
 		</div>
-  ); 
+	);
 }
