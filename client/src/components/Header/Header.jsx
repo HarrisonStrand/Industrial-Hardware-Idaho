b8f08f811e6584a72db0ea7a48f35a36d3648eab
Harrison Strand <harrisonstrand@STRAND.local>
2025-11-09 16:40:53 -0700

    shop now modal grid system, mapping, styling. Accessibility coloring

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -2,60 +2,67 @@ import { useEffect, useState, useContext } from "react";
 import { ThemeContext } from "../../context/ThemeContext.jsx";
 import { DataContext } from "../../context/DataContext";
 import { Link, useNavigate, useLocation } from "react-router-dom";
+import categories from "../../data/product-categories.json";
 import data from "../../data/products-test.json";
 import "./Header.css";
 
 export default function Header() {
 	const brand = useContext(DataContext);
-	
-  const navigate = useNavigate();
-  const location = useLocation();
 
-  const isProductListPage = location.pathname === "/products";
+	const navigate = useNavigate();
+	const location = useLocation();
 
-  // SEARCH
-  const [query, setQuery] = useState("");
+	const isProductListPage = location.pathname === "/products";
 
-  const handleSearch = (value) => {
-    setQuery(value);
+	// SHOP NOW HOVER MODAL
+	const [isModalOpen, setIsModalOpen] = useState(true);
+	const [activeTab, setActiveTab] = useState("Products");
 
-  if (location.pathname === "/products") {
-    const params = new URLSearchParams();
-    if (value.trim()) params.set("search", value.trim());
-    navigate(`/products?${params.toString()}`, { replace: true }); // 👈 updates URL without reload
-    return;
-  }
+	const tabs = Object.keys(categories);
 
-    const normalizedQuery = value.toLowerCase();
-    const filtered = data.products.filter((p) => {
-      const fields = [
-        p.name,
-        p.type,
-        p.grade,
-        p.material,
-        p.diameter,
-        p.length,
-        p.thread,
-        p.finish,
-      ].map((f) => f.toLowerCase());
-      return fields.some((f) => f.includes(normalizedQuery));
-    });
-  };
+	// SEARCH
+	const [query, setQuery] = useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+	const handleSearch = (value) => {
+		setQuery(value);
 
-    // 🔹 If we're on ProductList, do nothing special (live search handles it)
-    if (isProductListPage) return;
+		if (location.pathname === "/products") {
+			const params = new URLSearchParams();
+			if (value.trim()) params.set("search", value.trim());
+			navigate(`/products?${params.toString()}`, { replace: true }); // 👈 updates URL without reload
+			return;
+		}
 
-    // 🔹 If not on ProductList, redirect to filtered results
-    if (query.trim()) {
-      navigate(`/products?search=${encodeURIComponent(query.trim())}`);
-      setQuery("");
-    }
-  };
+		const normalizedQuery = value.toLowerCase();
+		const filtered = data.products.filter((p) => {
+			const fields = [
+				p.name,
+				p.type,
+				p.grade,
+				p.material,
+				p.diameter,
+				p.length,
+				p.thread,
+				p.finish,
+			].map((f) => f.toLowerCase());
+			return fields.some((f) => f.includes(normalizedQuery));
+		});
+	};
 
-// THEME BUTTON HANDLING
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		// 🔹 If we're on ProductList, do nothing special (live search handles it)
+		if (isProductListPage) return;
+
+		// 🔹 If not on ProductList, redirect to filtered results
+		if (query.trim()) {
+			navigate(`/products?search=${encodeURIComponent(query.trim())}`);
+			setQuery("");
+		}
+	};
+
+	// THEME BUTTON HANDLING
 	const { theme, toggleTheme } = useContext(ThemeContext);
 	const [logoSrc, setLogoSrc] = useState("");
 
@@ -79,39 +86,62 @@ export default function Header() {
 		setTimeout(updateLogoSrc, 0);
 	};
 
-
 	return (
 		<div className='header bg-main'>
-						<button
+			<button
 				onClick={handleToggleTheme}
 				className='theme-btn rounded-circle d-flex position-absolute'
 				style={{
 					backgroundColor: theme === "light" ? "#000" : "#fff",
-					transition: "background-color 0.3s ease, color 0.3s ease",}}>
-			</button>
-			<div className="container-fluid row g-0 justify-content-between px-3 py-2 align-items-center top-bar bg-secondary-light">
-				<div className="col-6 d-flex text-start justify-content-start">
-					<Link className="ms-4 d-flex text-decoration-none text-main align-items-center" to={`tel:${brand.phone}`}>
-						<div className="bi bi-telephone phone-icon text-main h5 m-0"/>
-						<h5 className="top-bar-text text-main font-secondary m-0 px-2">{brand.phone}</h5>
+					transition: "background-color 0.3s ease, color 0.3s ease",
+				}}></button>
+			<div className='container-fluid row g-0 justify-content-between px-3 py-2 align-items-center top-bar bg-secondary-light'>
+				<div className='col-6 d-flex text-start justify-content-start'>
+					<Link
+						className='ms-4 d-flex text-decoration-none text-main align-items-center'
+						to={`tel:${brand.phone}`}>
+						<div className='bi bi-telephone phone-icon text-main h5 m-0' />
+						<h5 className='top-bar-text text-main font-secondary m-0 px-2'>
+							{brand.phone}
+						</h5>
 					</Link>
-					<Link className="ms-4 d-flex text-decoration-none text-main align-items-center" to={`mailto:${brand.email}`}>
-						<div className="bi bi-envelope-open mail-icon text-main h5 m-0"/>
-						<h5 className="top-bar-text text-main font-secondary m-0 px-2">{brand.email}</h5>
+					<Link
+						className='ms-4 d-flex text-decoration-none text-main align-items-center'
+						to={`mailto:${brand.email}`}>
+						<div className='bi bi-envelope-open mail-icon text-main h5 m-0' />
+						<h5 className='top-bar-text text-main font-secondary m-0 px-2'>
+							{brand.email}
+						</h5>
 					</Link>
 				</div>
-				<div className="col-6 d-flex text-end justify-content-end">
-					<Link className="ms-4 d-flex text-decoration-none text-main align-items-center" to="/location">
-						<p className="top-bar-text text-main font-secondary text-uppercase m-0 px-2">Location</p>
+				<div className='col-6 d-flex text-end justify-content-end'>
+					<Link
+						className='ms-4 d-flex text-decoration-none text-main align-items-center'
+						to='/location'>
+						<p className='top-bar-text text-main font-secondary text-uppercase m-0 px-2'>
+							Location
+						</p>
 					</Link>
-					<Link className="ms-4 d-flex text-decoration-none text-main align-items-center" to="/about`">
-						<p className="top-bar-text text-main font-secondary text-uppercase m-0 px-2">About</p>
+					<Link
+						className='ms-4 d-flex text-decoration-none text-main align-items-center'
+						to='/about`'>
+						<p className='top-bar-text text-main font-secondary text-uppercase m-0 px-2'>
+							About
+						</p>
 					</Link>
-					<Link className="ms-4 d-flex text-decoration-none text-main align-items-center" to="/orders`">
-						<p className="top-bar-text text-main font-secondary text-uppercase m-0 px-2">orders</p>
+					<Link
+						className='ms-4 d-flex text-decoration-none text-main align-items-center'
+						to='/orders`'>
+						<p className='top-bar-text text-main font-secondary text-uppercase m-0 px-2'>
+							orders
+						</p>
 					</Link>
-					<Link className="ms-4 d-flex text-decoration-none text-main align-items-center" to="/careers`">
-						<p className="top-bar-text text-main font-secondary text-uppercase m-0 px-2">careers</p>
+					<Link
+						className='ms-4 d-flex text-decoration-none text-main align-items-center'
+						to='/careers`'>
+						<p className='top-bar-text text-main font-secondary text-uppercase m-0 px-2'>
+							careers
+						</p>
 					</Link>
 				</div>
 			</div>
@@ -134,7 +164,7 @@ export default function Header() {
 						<Link className='account-link rounded-circle mx-4' to='/register'>
 							<div className='account-thumb rounded-circle' />
 						</Link>
-						<div className='cart-icon py-0 my-0 d-none d-sm-flex text-main-light bi bi-bag h2'/>
+						<div className='cart-icon py-0 my-0 d-none d-sm-flex text-main-light bi bi-bag h2' />
 					</div>
 					<div className='col-2 text-end d-none'>
 						<button
@@ -147,27 +177,86 @@ export default function Header() {
 					</div>
 				</div>
 			</nav>
-			<div className="container-fluid row g-0 justify-content-between px-3 py-2 align-items-center search-bar-container bg-secondary">
-				<div className="col-6 d-flex text-start justify-content-start">
-					<Link className="ms-4 d-flex text-decoration-none text-secondary-light align-items-center" to="/products">
-						<p className="search-bar-text text-secondary-light font-secondary fw-bold text-uppercase m-0 px-2">Shop Now</p>
-						<div className="bi bi-chevron-down chevron text-secondary-light fw-bold h5 m-0"/>
+			<div className='container-fluid row g-0 justify-content-between px-3 py-2 align-items-center search-bar-container bg-secondary'>
+				<div className='col-6 d-flex text-start justify-content-start'>
+					<div
+						className='ms-4 d-flex text-decoration-none text-secondary-light align-items-center'
+						to='/products'>
+						<div
+							className='shop-now-wrapper position-relative'
+							onMouseEnter={() => setIsModalOpen(true)}
+							onMouseLeave={() => setIsModalOpen(false)}>
+							<div className='shop-now-trigger d-flex align-items-center position-relative z-10'>
+								<p className='search-bar-text text-secondary-light font-secondary fw-bold text-uppercase m-0 px-2'>
+									Shop Now
+								</p>
+								<i className='bi bi-chevron-down chevron text-secondary-light fw-bold h5 m-0' />
+							</div>
+							{isModalOpen && (
+								<div className='category-modal position-absolute rounded-2 py-1'>
+									{/* Invisible bridge area */}
+									<div className='modal-hover-bridge'></div>
+
+									<div className='d-flex border-bottom border-main mb-0 justify-content-between'>
+										{tabs.map((tab) => (
+											<button
+												key={tab}
+												onMouseEnter={() => setActiveTab(tab)}
+												className={`mega-tab text-uppercase fs-6 border-0 bg-transparent pb-0 ${
+													activeTab === tab ? "active" : ""
+												}`}>
+												{tab}
+											</button>
+										))}
+									</div>
+
+									<div className='category-links-wrapper d-flex row justify-content-start align-items-center'>
+										{categories[activeTab].map((item) => (
+											<Link
+												key={item.id}
+												to={item.path}
+												className='d-inline-block text-uppercase text-start text-decoration-none py-1 pe-2 category-link col-4'>
+												{item.name}
+											</Link>
+										))}
+									</div>
+								</div>
+							)}
+						</div>
+					</div>
+					<div className='search-bar-text-line px-3'></div>
+					<Link
+						className='ms-4 d-flex text-decoration-none text-secondary-light align-items-center'
+						to='/products`'>
+						<p className='search-bar-text text-secondary-light font-secondary fw-bold text-uppercase m-0 px-2'>
+							Products
+						</p>
 					</Link>
-					<div className="search-bar-text-line px-3"></div>
-					<Link className="ms-4 d-flex text-decoration-none text-secondary-light align-items-center" to="/products`">
-						<p className="search-bar-text text-secondary-light font-secondary fw-bold text-uppercase m-0 px-2">Products</p>
-					</Link>
-					<div className="search-bar-text-line px-3"></div>
-					<Link className="ms-4 d-flex text-decoration-none text-secondary-light align-items-center" to="/contact`">
-						<p className="search-bar-text text-secondary-light font-secondary fw-bold text-uppercase m-0 px-2">contact</p>
+					<div className='search-bar-text-line px-3'></div>
+					<Link
+						className='ms-4 d-flex text-decoration-none text-secondary-light align-items-center'
+						to='/contact`'>
+						<p className='search-bar-text text-secondary-light font-secondary fw-bold text-uppercase m-0 px-2'>
+							contact
+						</p>
 					</Link>
 				</div>
-				<div className="col-6 d-flex text-end justify-content-end">
-					<form onSubmit={handleSubmit} className="d-flex position-relative w-100">
-						<input type="text" className="search-bar me-4 d-flex w-100 fw-lighter ps-3 pe-5 align-items-center" value={query} onChange={(e) => handleSearch(e.target.value)} placeholder="Search Products"/>
-					<button type="submit" className="btn position-absolute end-0 top-50 translate-middle pt-0 pe-2 me-1 border-0 bg-transparent">
-						<i className="bi bi-search fs-5"></i>
-					</button>
+				<div className='col-6 d-flex text-end justify-content-end'>
+					<form
+						onSubmit={handleSubmit}
+						className='d-flex position-relative w-100'>
+						<input
+							type='text'
+							className='search-bar me-4 d-flex w-100 fw-lighter ps-3 pe-5 align-items-center'
+							value={query}
+							onChange={(e) => handleSearch(e.target.value)}
+							placeholder='Search Products'
+						/>
+						<button
+							type='submit'
+							className='btn position-absolute end-0 top-50 translate-middle pt-0 pe-2 me-1 border-0 bg-transparent'>
+							<i className='bi bi-search fs-5'></i>
+						</button>
 					</form>
 				</div>
 			</div>
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -4,11 +4,8 @@ import "./Home.css";
 
 export default function Home() {
 	return (
-	<>
-	<section className="container-fluid py-0 heroMain py-lg-0 pb-3 px-0">
+	<section className="container-fluid py-0 heroMain py-lg-0 pb-3 px-0 bg-dark">
 		<ProductCategories/>
 	</section>
-
-	</>
 	);
 }
