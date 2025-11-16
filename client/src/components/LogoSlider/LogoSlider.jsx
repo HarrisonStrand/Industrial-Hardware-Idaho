cafb9cf985702cdf4f90ff6ef676a23ea964399b
Harrison Strand <harrisonstrand@STRAND.local>
2025-11-16 09:20:34 -0700

    fix all logoslider styling

--- client/src/components/LogoSlider/LogoSlider.jsx
+++ client/src/components/LogoSlider/LogoSlider.jsx
@@ -3,17 +3,19 @@ import brandLogos from "../../data/brand-logos.json";
 
 export default function LogoSlider() {
   return (
-    <div className="logo-slider-container">
+    <div className="logo-slider-container mb-5 py-2">
+				<div className="logo-slider-overlay"></div>
       <div className="logo-track">
-				<div className="logo-track-overlay">
+
         {brandLogos.logos.map((logo, i) => (
-					<img key={i} src={logo.src} alt={logo.alt} className="logo-item" />
+          <img key={i} src={logo.src} alt={logo.alt} className="logo-item" />
         ))}
+
         {/* Duplicate set for seamless looping */}
         {brandLogos.logos.map((logo, i) => (
-					<img key={`dup-${i}`} src={logo.src} alt={logo.alt} className="logo-item" />
+          <img key={`dup-${i}`} src={logo.src} alt={logo.alt} className="logo-item" />
         ))}
-				</div>
+
       </div>
     </div>
   );
