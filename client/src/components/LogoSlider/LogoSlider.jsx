65096536d59258a20fa6c6ea6fd47319c95d3f86
Harrison Strand <harrisonstrand@STRAND.local>
2025-11-16 09:06:17 -0700

    add speed and gradient to company slider

--- client/src/components/LogoSlider/LogoSlider.jsx
+++ client/src/components/LogoSlider/LogoSlider.jsx
@@ -5,17 +5,15 @@ export default function LogoSlider() {
   return (
     <div className="logo-slider-container">
       <div className="logo-track">
-
-        {/* First track */}
+				<div className="logo-track-overlay">
         {brandLogos.logos.map((logo, i) => (
-          <img key={i} src={logo.src} alt={logo.alt} className="logo-item" />
+					<img key={i} src={logo.src} alt={logo.alt} className="logo-item" />
         ))}
-
-        {/* Cloned track (must be identical!) */}
+        {/* Duplicate set for seamless looping */}
         {brandLogos.logos.map((logo, i) => (
-          <img key={`dup-${i}`} src={logo.src} alt={logo.alt} className="logo-item" />
+					<img key={`dup-${i}`} src={logo.src} alt={logo.alt} className="logo-item" />
         ))}
-
+				</div>
       </div>
     </div>
   );
