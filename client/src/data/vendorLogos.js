8e92313757f23b82012f2907bb8045157d250574
Harrison Strand <harrisonstrand@gmail.com>
2026-08-10 21:38:50 -0600

    add new vendor logos to carousel and vendor information page

--- client/src/data/vendorLogos.js
+++ client/src/data/vendorLogos.js
@@ -1,118 +1,33 @@
 const vendorLogos = [
 	{
-		name: "Apex",
-		logo: "/vendor-logos/Apex.png",
-		website: "https://www.apextoolgroup.com/",
-	},
-	{
-		name: "Auveco",
-		logo: "/vendor-logos/Auveco.png",
-		website: "https://www.auveco.com",
-	},
-	{
-		name: "BDI Express",
-		logo: "/vendor-logos/BDI.png",
-		website: "https://www.bdiexpress.com",
-	},
-	{
 		name: "Brighton",
 		logo: "/vendor-logos/Brighton-Best.png",
 		website: "https://www.brightonbest.com",
 	},
 	{
-		name: "Delcity",
-		logo: "/vendor-logos/Del-City.png",
-		website: "https://www.delcity.net",
+		name: "Intercorp",
+		logo: "/vendor-logos/Intercorp.png",
+		website: "https://intercorpusa.com/home",
 	},
 	{
-		name: "Dewalt",
-		logo: "/vendor-logos/Dewalt.png",
-		website: "https://www.dewalt.com/en-us",
+		name: "Makita",
+		logo: "/vendor-logos/Makita.png",
+		website: "https://makitatools.com",
 	},
 	{
 		name: "Diablo Tools",
 		logo: "/vendor-logos/Diablo.png",
 		website: "https://www.diablotools.com",
 	},
-	// {
-	// 	name: "Durham Manufacturing.png",
-	// 	logo: "/vendor-logos/Durham.png",
-	// 	website: "https://www.durhammfg.com",
-	// },
 	{
-		name: "EZ Lok",
-		logo: "/vendor-logos/EZ-Lok.png",
-		website: "https://www.ezlok.com",
+		name: "Big Timber",
+		logo: "/vendor-logos/Big-timber.png",
+		website: "https://www.marmonfasteners.com/big-timber-us",
 	},
 	{
-		name: "Global Industrial",
-		logo: "/vendor-logos/Global-Industries.png",
-		website: "https://www.globalindustrial.com",
-	},
-	{
-		name: "Highline Warren",
-		logo: "/vendor-logos/Highline-Warren.png",
-		website: "https://www.shophighlinewarren.com",
-	},
-	{
-		name: "Hillman",
-		logo: "/vendor-logos/Hillman.png",
-		website: "https://www.hillmangroup.com",
-	},
-	{
-		name: "Hillsdale Terminal",
-		logo: "/vendor-logos/Hillsdale.png",
-		website: "https://www.hillsdaleterminal.com",
-	},
-	{
-		name: "Hilti",
-		logo: "/vendor-logos/Hilti.png",
-		website: "https://www.hilti.com",
-	},
-	{
-		name: "Howmet",
-		logo: "/vendor-logos/Howmet.png",
-		website: "https://www.afsindustrial.com",
-	},
-	{
-		name: "Huyett",
-		logo: "/vendor-logos/Huyett.png",
-		website: "https://www.huyett.com",
-	},
-	{
-		name: "Kanebridge",
-		logo: "/vendor-logos/Kanebridge.png",
-		website: "https://www.kanebridge.com",
-	},
-	{
-		name: "Levelset",
-		logo: "/vendor-logos/Levelset.png",
-		website: "https://www.levelset.com",
-	},
-	{
-		name: "Louisville Ladder",
-		logo: "/vendor-logos/Louiville.png",
-		website: "https://www.louisvilleladder.com",
-	},
-	{
-		name: "Majestic",
-		logo: "/vendor-logos/Majestic.png",
-		website: "https://www.majesticglove.com",
-	},
-	{
-		name: "McMaster-Carr",
-		logo: "/vendor-logos/McMaster-Carr.png",
-		website: "https://www.mcmaster.com",
-	},
-	{
-		name: "ORS Nasco",
-		logo: "/vendor-logos/ORS-Nasco.png",
-		website: "https://www.orsnasco.com",
-	},
-	{
-		name: "Procore",
-		logo: "/vendor-logos/Procore.png",
-		website: "https://www.procore.com",
+		name: "Proferred",
+		logo: "/vendor-logos/Proferred.png",
+		website: "https://www.proferredscrews.com",
 	},
 	{
 		name: "Simpson Strongtie",
@@ -120,10 +35,135 @@ const vendorLogos = [
 		website: "https://www.strongtie.com",
 	},
 	{
-		name: "Waytek ",
-		logo: "/vendor-logos/Waytek.png",
-		website: "https://www.waytekwire.com",
+		name: "Dewalt/Powers",
+		logo: "/vendor-logos/Dewalt.png",
+		website: "https://www.dewalt.com/en-us",
 	},
+	{
+		name: "Norseman",
+		logo: "/vendor-logos/Norseman.png",
+		website: "https://norsemandrill.com",
+	},
+	{
+		name: "CRC",
+		logo: "/vendor-logos/Crc.png",
+		website: "https://www.crcindustries.com",
+	},
+	{
+		name: "PHD",
+		logo: "/vendor-logos/Phd.png",
+		website: "https://www.phd-mfg.com",
+	},
+	{
+		name: "CGW",
+		logo: "/vendor-logos/Cgw.png",
+		website: "https://www.cgwabrasives.com",
+	},
+	// {
+	// 	name: "Apex",
+	// 	logo: "/vendor-logos/Apex.png",
+	// 	website: "https://www.apextoolgroup.com/",
+	// },
+	{
+		name: "Auveco",
+		logo: "/vendor-logos/Auveco.png",
+		website: "https://www.auveco.com",
+	},
+	// {
+	// 	name: "BDI Express",
+	// 	logo: "/vendor-logos/BDI.png",
+	// 	website: "https://www.bdiexpress.com",
+	// },
+	// {
+	// 	name: "Delcity",
+	// 	logo: "/vendor-logos/Del-City.png",
+	// 	website: "https://www.delcity.net",
+	// },
+	// {
+	// 	name: "Durham Manufacturing.png",
+	// 	logo: "/vendor-logos/Durham.png",
+	// 	website: "https://www.durhammfg.com",
+	// },
+	// {
+	// 	name: "EZ Lok",
+	// 	logo: "/vendor-logos/EZ-Lok.png",
+	// 	website: "https://www.ezlok.com",
+	// },
+	// {
+	// 	name: "Global Industrial",
+	// 	logo: "/vendor-logos/Global-Industries.png",
+	// 	website: "https://www.globalindustrial.com",
+	// },
+	// {
+	// 	name: "Highline Warren",
+	// 	logo: "/vendor-logos/Highline-Warren.png",
+	// 	website: "https://www.shophighlinewarren.com",
+	// },
+	// {
+	// 	name: "Hillman",
+	// 	logo: "/vendor-logos/Hillman.png",
+	// 	website: "https://www.hillmangroup.com",
+	// },
+	// {
+	// 	name: "Hillsdale Terminal",
+	// 	logo: "/vendor-logos/Hillsdale.png",
+	// 	website: "https://www.hillsdaleterminal.com",
+	// },
+	// {
+	// 	name: "Hilti",
+	// 	logo: "/vendor-logos/Hilti.png",
+	// 	website: "https://www.hilti.com",
+	// },
+	// {
+	// 	name: "Howmet",
+	// 	logo: "/vendor-logos/Howmet.png",
+	// 	website: "https://www.afsindustrial.com",
+	// },
+	{
+		name: "Huyett",
+		logo: "/vendor-logos/Huyett.png",
+		website: "https://www.huyett.com",
+	},
+	// {
+	// 	name: "Kanebridge",
+	// 	logo: "/vendor-logos/Kanebridge.png",
+	// 	website: "https://www.kanebridge.com",
+	// },
+	// {
+	// 	name: "Levelset",
+	// 	logo: "/vendor-logos/Levelset.png",
+	// 	website: "https://www.levelset.com",
+	// },
+	// {
+	// 	name: "Louisville Ladder",
+	// 	logo: "/vendor-logos/Louiville.png",
+	// 	website: "https://www.louisvilleladder.com",
+	// },
+	// {
+	// 	name: "Majestic",
+	// 	logo: "/vendor-logos/Majestic.png",
+	// 	website: "https://www.majesticglove.com",
+	// },
+	// {
+	// 	name: "McMaster-Carr",
+	// 	logo: "/vendor-logos/McMaster-Carr.png",
+	// 	website: "https://www.mcmaster.com",
+	// },
+	// {
+	// 	name: "ORS Nasco",
+	// 	logo: "/vendor-logos/ORS-Nasco.png",
+	// 	website: "https://www.orsnasco.com",
+	// },
+	// {
+	// 	name: "Procore",
+	// 	logo: "/vendor-logos/Procore.png",
+	// 	website: "https://www.procore.com",
+	// },
+	// {
+	// 	name: "Waytek ",
+	// 	logo: "/vendor-logos/Waytek.png",
+	// 	website: "https://www.waytekwire.com",
+	// },
 	{
 		name: "Werner",
 		logo: "/vendor-logos/Werner.png",
